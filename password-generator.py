f101fe44071565bbce6bce94ae40605a9cd25714
Ron <100487379+NixeFR@users.noreply.github.com>
2022-03-04 17:41:54 +0100

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,20 +1,14 @@
-#Import the module random
 import random
-#Import the module string
 import string 
 
-#Creating our password generator function
 def pass_gen(length, number):
-    #Creating an empty list in case we need multiple passwords
+    # Creating a list just in case the user would like to have multiple passwords at once.
     passwords = []
-    #Creating an empty password variable
     password = ''
-    #Main loop that generates our password(s).
     for _ in range(number):
         for _ in range(length): password += random.choice(string.ascii_letters + string.digits + string.punctuation)
         passwords.append(password)
         password = ''
     return passwords
 
-#Printing our result
 print(pass_gen(32,1))
